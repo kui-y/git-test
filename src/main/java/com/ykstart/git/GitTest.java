bfec3040a50f0dcf6d0711dbc078e4ba2a1fa58f
ykstart <git@ykstart.com>
2022-06-18 00:21:37 +0800

    first commit

---
+++ src/main/java/com/ykstart/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ykstart.git;
+
+/**
+ * @author ykstart
+ * @create 2022-06-18 0:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
