5745cea39af4a73dbc8bf840116381b5ef9edd21
ykstart <git@ykstart.com>
2022-06-18 17:46:13 +0800

    third commit

--- src/main/java/com/ykstart/git/GitTest.java
+++ src/main/java/com/ykstart/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello!");
         System.out.println("hello 你好!");
         System.out.println("hello master!");
+        System.out.println("hello push!");
     }
 }
