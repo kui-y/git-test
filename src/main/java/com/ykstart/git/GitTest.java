e4ae6839d9e66417279109ae8f9b0333ca9fa841
ykstart <git@ykstart.com>
2022-06-18 10:36:59 +0800

    master first commit

--- src/main/java/com/ykstart/git/GitTest.java
+++ src/main/java/com/ykstart/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello!");
         System.out.println("hello 你好!");
+        System.out.println("hello master!");
     }
 }
